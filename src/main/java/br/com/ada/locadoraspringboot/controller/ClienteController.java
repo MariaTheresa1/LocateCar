03d40b4ed940eea02c3507975b29a901094795c0
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-03-10 15:18:10 -0300

    método listarPorId

--- src/main/java/br/com/ada/locadoraspringboot/controller/ClienteController.java
+++ src/main/java/br/com/ada/locadoraspringboot/controller/ClienteController.java
@@ -2,10 +2,14 @@ package br.com.ada.locadoraspringboot.controller;
 
 import br.com.ada.locadoraspringboot.model.Cliente;
 import br.com.ada.locadoraspringboot.service.ClienteService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cliente")
@@ -24,4 +28,13 @@ public class ClienteController {
         return this.clienteService.listarTodos();
     }
 
+    @GetMapping("/by/{id}")
+    public ResponseEntity<Cliente> buscarClientePor(@PathVariable("id") Long id) {
+        Optional<Cliente> optionalCliente = this.clienteService.buscarClientePorId(id);
+
+        if(optionalCliente.isPresent()){
+            return ResponseEntity.ok(optionalCliente.get());
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- src/main/java/br/com/ada/locadoraspringboot/service/ClienteService.java
+++ src/main/java/br/com/ada/locadoraspringboot/service/ClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -19,5 +20,9 @@ public class ClienteService {
 
     public List<Cliente> listarTodos() {
         return this.clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarClientePorId(Long id) {
+        return this.clienteRepository.findById(id);
     }
 }
